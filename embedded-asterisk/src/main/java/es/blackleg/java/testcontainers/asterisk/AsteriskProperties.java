bd36ae7c53923ca7060eb31a81abba36c7b54c64
Hector Espert <hectorespertpardo@gmail.com>
2023-07-18 16:33:33 +0200

    Change image and add hints

--- embedded-asterisk/src/main/java/es/blackleg/java/testcontainers/asterisk/AsteriskProperties.java
+++ embedded-asterisk/src/main/java/es/blackleg/java/testcontainers/asterisk/AsteriskProperties.java
@@ -3,10 +3,10 @@ package es.blackleg.java.testcontainers.asterisk;
 import com.playtika.testcontainer.common.properties.CommonContainerProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties("embedded.prometheus")
+@ConfigurationProperties("embedded.asterisk")
 public class AsteriskProperties extends CommonContainerProperties {
     @Override
     public String getDefaultDockerImage() {
-        return "blackleg/asterisk:b32e7be";
+        return "blackleg/asterisk:edge";
     }
 }
